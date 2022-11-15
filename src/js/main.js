f51f5e86125f491269f970267c2cfaf86057a280
Allen Chang <allen.ty.chang@gmail.com>
2022-11-15 11:45:23 -0500

    add legend for happiness score

--- src/js/main.js
+++ src/js/main.js
@@ -73,6 +73,7 @@ svg
   );
 
 const colors = d3.interpolateOrRd;
+createLegend();
 
 const earth = svg.append("g");
 
@@ -111,4 +112,30 @@ function fillLand(d) {
   } else {
     return "darkgrey";
   }
+}
+
+function createLegend() {
+  const legendWidth = 150;
+  const legendHeight = 20;
+
+  const cScale = d3.scaleSequential().interpolator(colors).domain([0, 99]);
+  const xScale = d3.scaleLinear().domain([0, 99]).range([0, 150]);
+
+  d3.select("#legendBar")
+    .attr("width", legendWidth)
+    .attr("height", legendHeight)
+    .selectAll("rect")
+    .data(Array.from(Array(100).keys()))
+    .enter()
+    .append("rect")
+    .attr("x", (d) => Math.floor(xScale(d)))
+    .attr("y", 0)
+    .attr("height", 20)
+    .attr("width", (d) => {
+      if (d == 99) {
+        return 6;
+      }
+      return Math.floor(xScale(d + 1)) - Math.floor(xScale(d)) + 1;
+    })
+    .attr("fill", (d) => cScale(d));
 }
