b3c7146dfaa254507677d89599c6a99b70ec3a46
Allen Chang <allen.ty.chang@gmail.com>
2022-11-14 22:30:40 -0500

    add bootstrap config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-const path = require("path");
+import { defineConfig } from "vite";
+import path from "path";
 
-export default {
+export default defineConfig({
   root: path.resolve(__dirname, "src"),
   resolve: {
     alias: {
@@ -11,4 +12,4 @@ export default {
     port: 8080,
     hot: true,
   },
-};
+});
