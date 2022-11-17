132637b16049773198c0e962c82111c7bb0ba8ef
Allen Chang <allen.ty.chang@gmail.com>
2022-11-16 22:49:58 -0500

    2022.11.16_v3

--- src/js/scatter.js
+++ src/js/scatter.js
@@ -1,8 +1,15 @@
 import * as d3 from "d3";
 
 import worldHappiness from "../public/datasets/world-happiness.json";
-const rawdata = worldHappiness["2015"];
-let index = "Economy (GDP per Capita)";
+const defaultData = [];
+for (const [_, info] of Object.entries(worldHappiness["2015"])) {
+  defaultData.push(info);
+}
+
+const body = document.getElementById("scatterplot");
+let graphWidth = body.clientWidth - 150,
+  graphHeight = body.clientHeight - 150;
+const margin = 100;
 
 const color = d3.schemeTableau10;
 const regions = [
@@ -16,85 +23,148 @@ const regions = [
   "Eastern Asia",
   "Sub-Saharan Africa",
   "Southern Asia",
-  "All",
+  "Select all region",
 ];
+const svg = d3.select("#scatterplot").append("svg");
 
-export default function initScatter() {
+const graph = svg
+  .append("g")
+  .attr("position", "relative")
+  .attr("transform", `translate(${margin}, ${margin})`);
+
+const xScale = d3.scaleLinear().domain([0, 2]).range([0, graphWidth]);
+const xAxis = d3.axisBottom(xScale).ticks(20);
+const xAxisGroup = graph
+  .append("g")
+  .attr("transform", `translate(0, ${graphHeight})`)
+  .call(xAxis);
+
+const yScale = d3.scaleLinear().domain([2, 0]).range([0, graphHeight]);
+const yAxis = d3.axisLeft(yScale).ticks(20);
+const yAxisGroup = graph.append("g").call(yAxis);
+
+const xLabel = graph
+  .append("g")
+  .append("text")
+  .attr("class", "x-axis-label")
+  .attr("y", graphHeight + 50)
+  .attr("x", graphWidth / 2)
+  .attr("font-size", "18px")
+  .attr("font-weight", "600")
+  .attr("text-anchor", "middle")
+  .attr("fill", "white")
+  .text("GDP Per Capita ($)");
+
+const yLabel = graph
+  .append("g")
+  .append("text")
+  .attr("class", "yAxisGroup")
+  .attr("transform", "rotate(-90)")
+  .attr("x", -(graphHeight / 2))
+  .attr("y", -50)
+  .attr("font-size", "18px")
+  .attr("font-weight", "600")
+  .attr("text-anchor", "middle")
+  .attr("fill", "white")
+  .text("Happiness Score");
+
+graph
+  .append("g")
+  .selectAll("circle")
+  .data(defaultData)
+  .enter()
+  .append("circle")
+  .attr(
+    "class",
+    (d) =>
+      `country-${d["Country"]} region-${d["Region"]
+        .split(" ")
+        .join("-")} country-circle`
+  )
+  .attr("cx", (d) => xScale(d["Economy (GDP per Capita)"]))
+  .attr("cy", (d) => yScale(d["Happiness Score"]))
+  .attr("fill", (d) => {
+    const idx = regions.indexOf(d["Region"]);
+    return color[idx];
+  })
+  .attr("r", "5");
+
+const legend = graph
+  .selectAll(".legend")
+  .data(regions)
+  .enter()
+  .append("g")
+  .attr("class", "legend")
+  .attr("position", "absolute")
+  .attr("transform", `translate(${graphWidth - margin}, ${0})`);
+
+legend
+  .append("rect")
+  .attr("x", 0)
+  .attr("y", function (d, i) {
+    return 20 * i;
+  })
+  .attr("width", 20)
+  .attr("height", 20)
+  .style("fill", function (d) {
+    const idx = regions.indexOf(d);
+    return color[idx];
+  })
+  .text(function (d) {
+    return d;
+  });
+
+legend
+  .append("text")
+  .attr("x", 25)
+  .attr("text-anchor", "start")
+  .attr("y", function (d, i) {
+    return 20 * i;
+  })
+  .attr("dy", "1.15em")
+  .text(function (d) {
+    return d;
+  })
+  .attr("font-size", "12px")
+  .style("fill", "white");
+
+export default function initScatter(index, year) {
+  graphWidth = body.clientWidth - 150;
+  graphHeight = body.clientHeight - 150;
+  svg.attr(
+    "viewBox",
+    `0 0 ${graphWidth + margin * 2} ${graphHeight + margin * 2}`
+  );
+
+  xLabel
+    .attr("y", graphHeight + 50)
+    .attr("x", graphWidth / 2)
+    .text(index);
+  yLabel.attr("x", -(graphHeight / 2));
+
+  legend.attr("transform", `translate(${graphWidth - margin}, ${0})`);
+
   const data = [];
-  for (const [_, info] of Object.entries(rawdata)) {
+  for (const [_, info] of Object.entries(worldHappiness[year])) {
     data.push(info);
   }
 
-  const body = document.getElementById("scatterplot");
-  const graphWidth = body.clientWidth - 150,
-    graphHeight = body.clientHeight - 150,
-    margin = 100;
-
-  const svg = d3
-    .select("#scatterplot")
-    .append("svg")
-    .attr(
-      "viewBox",
-      `0 0 ${graphWidth + margin * 2} ${graphHeight + margin * 2}`
-    );
-
-  const graph = svg
-    .append("g")
-    .attr("position", "relative")
-    .attr("transform", `translate(${margin}, ${margin})`);
-
   const xMax = Math.ceil(d3.max(data, (d) => d[index]));
-  const x = d3.scaleLinear().domain([0, xMax]).range([0, graphWidth]);
-
-  const xAxis = d3.axisBottom(x).ticks(20);
-
-  const xAxisGroup = graph
-    .append("g")
+  xScale.domain([0, xMax]).range([0, graphWidth]);
+  xAxisGroup
     .attr("transform", `translate(0, ${graphHeight})`)
-    .call(xAxis)
-    .attr("class", "x-axis");
+    .transition()
+    .call(xAxis);
 
   const yMax = Math.ceil(d3.max(data, (d) => d["Happiness Score"]));
-  const y = d3.scaleLinear().domain([yMax, 0]).range([0, graphHeight]);
+  yScale.domain([yMax, 0]).range([0, graphHeight]);
+  yAxisGroup.transition().call(yAxis);
 
-  const yAxis = d3.axisLeft(y).ticks(20);
-
-  const yAxisGroup = graph.append("g").call(yAxis);
-
-  const xLabel = graph
-    .append("g")
-    .append("text")
-    .attr("class", "x-axis-label")
-    .attr("y", graphHeight + 50)
-    .attr("x", graphWidth / 2)
-    .attr("font-size", "18px")
-    .attr("font-weight", "600")
-    .attr("text-anchor", "middle")
-    .text("GDP Per Capita ($)");
-
-  const yLabel = graph
-    .append("g")
-    .append("text")
-    .attr("class", "yAxisGroup")
-    .attr("transform", "rotate(-90)")
-    .attr("x", -(graphHeight / 2))
-    .attr("y", -50)
-    .attr("font-size", "18px")
-    .attr("font-weight", "600")
-    .attr("text-anchor", "middle")
-    .text("Happiness Index (%)");
-
-  const circles = graph
-    .append("g")
-    .selectAll("circle")
+  graph
+    .selectAll(".country-circle")
     .data(data)
-    .enter()
-    .append("circle")
-    .attr("cx", (d) => x(d[index]))
-    .attr("cy", (d) => y(d["Happiness Score"]))
-    .attr("fill", (d) => {
-      const idx = regions.indexOf(d["Region"]);
-      return color[idx];
-    })
-    .attr("r", "5");
+    .transition()
+    .duration(750)
+    .attr("cx", (d) => xScale(d[index]))
+    .attr("cy", (d) => yScale(d["Happiness Score"]));
 }
